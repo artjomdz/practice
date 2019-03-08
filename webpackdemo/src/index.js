331a7dda828fc36461904826f8789f5a7568eca8
Artjom Dzug <artjomdz@gmail.com>
2019-03-08 15:02:58 +0200

    React test

---
+++ webpackdemo/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Message from './message.jsx';
+import './index.html';
+
+ReactDOM.render(<Message />, document.querySelector('#app'));
---
+++ webpackdemo/src/message.jsx
@@ -0,0 +1,10 @@
+import './message.css';
+import React from 'react';
+
+export default class Message extends React.Component {
+    render() {
+        return <h1>Hello Webpack</h1>
+    }
+}
+
+
